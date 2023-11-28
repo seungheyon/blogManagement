4509d6a08d1789f5740a6ff03b633f278f89906a
LSH <fhzktm@naver.com>
2023-11-28 14:26:44 +0900

    Initial commit

---
+++ src/test/java/com/example/blogmanagement/BlogManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
